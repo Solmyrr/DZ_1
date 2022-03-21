daaa97a622304ab772738dde06aec2c180beabde
Solmyrr <bratishcheva_dom@mail.ru>
2022-03-21 15:56:22 +0300

    adding files

---
+++ task_1_3.py
@@ -0,0 +1,10 @@
+
+percent = range(1, 21, 1)
+for number in percent:
+    if number == 1:
+        print(number, 'процент')
+    elif 2 <= number <=4:
+        print(number, "процента")
+    else:
+        print(number, "процентов")
+# Задача не сложная
